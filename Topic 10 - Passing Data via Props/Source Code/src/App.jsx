573c470642239e5d2bd76c477c4adfc8d618c341
Jawad Sher <jawadsher04@gmail.com>
2024-04-30 13:00:18 +0500

    Adding 10th Topic Passing Data via Props

---
+++ Topic 10 - Passing Data via Props/Source Code/src/App.jsx
@@ -0,0 +1,18 @@
+import FoodItems from "./Components/FoodItems";
+import ErrorMessg from "./Components/ErrorMessg";
+function App() {
+  // let foodItems = ["Daal", "Bindi", "Sugar", "Roti", "Milk", "Banana"];
+  let foodItems = [];
+
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ErrorMessg foodItems={foodItems}></ErrorMessg>
+      <FoodItems foodItems={foodItems}></FoodItems>
+    </>
+  );
+}
+
+export default App;
+
+
